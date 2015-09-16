50dcad04c1f6ea325df8a0fa89cb2cd2472df2b1
Byron Roark <byronroark@gmail.com>
2015-09-16 17:28:29 -0400

    updated tictactoe, created player methods, working now

--- tictactoe.rb
+++ tictactoe.rb
@@ -1,14 +1,10 @@
 require_relative 'player'
+require 'pry'
 
 class TicTacToe
   def initialize
     @spaces = [1,2,3,4,5,6,7,8,9]
-
-    @player_1_choice = 0
-    @player_2_choice = 0
-    @count = 0
-
-    @winning_row = [
+    @winning_rows = [
       [1,2,3],
       [4,5,6],
       [7,8,9],
@@ -18,6 +14,7 @@ class TicTacToe
       [1,5,9],
       [3,5,7]
     ]
+      @turn_count = 0
   end
 
   def game_board
@@ -34,44 +31,72 @@ class TicTacToe
     @spaces[position-1] != "o" && @spaces[position-1] != "x"
   end
 
-  # define player method
-  # count += 1
+  def player_1_turn
+    game_board
+    puts "Make your move #{@player_1.name.upcase}:"
+    player_choice = gets.chomp.to_i
+    until valid_move?(player_choice)
+      puts "Invalid choice.. please make another choice!"
+      player_choice = gets.chomp.to_i
+    end
+    @spaces[player_choice - 1] = "X"
+    game_board
+    @turn_count += 1
+    print "Round: #{@turn_count}\n"
+  end
+
+  def player_2_turn
+    game_board
+    puts "Make your move #{@player_2.name.upcase}:"
+    player_choice = gets.chomp.to_i
+    until valid_move?(player_choice)
+      puts "Invalid choice.. please make another choice!"
+      player_choice = gets.chomp.to_i
+    end
+    @spaces[player_choice - 1] = "O"
+    game_board
+    @turn_count += 1
+    print "Round: #{@turn_count}\n"
+  end
 
   def start_game
     multi_player_mode
   end
 
   def play
-    multi_player_mode
-    while @count < 9
-      game_board
+    while @turn_count < 7
+      player_1_turn
+      if winner_chicken_dinner('X')
+        puts "Winner Winner Chicken Dinner! #{@player_1.name.upcase} WINS!"
+        exit
+      end
+      player_2_turn
+      if winner_chicken_dinner('O')
+        puts "Winner Winner Chicken Dinner! #{@player_2.name.upcase} WINS!"
+        exit
+      end
     end
-    winner_chicken_dinner
+    puts "It's a TIE!"
   end
 
-  # def winner_chicken_dinner
-  #   if @winning_rows.any? { |winning_row| winning_rows.all? { } }
-  # end
-
-  def player_wins(x_or_o)
-    if @spaces[0] == "X" && @spaces[1] == "X" && @spaces[2] == "X"
-      return true
-    end
+  def winner_chicken_dinner(xo)
+    @winning_rows.any? { |winning_row|
+      winning_row.all? { |position| @spaces[position-1] == xo } }
   end
 
   # def single_player_mode
   # end
 
   def multi_player_mode
-    puts "Player 1, enter your name:"
+    puts "Player 1, enter your Name:"
     name = gets.chomp
-    player_1 = Player.new(name)
-    puts "#{player_1.name.upcase} will be X."
+    @player_1 = Player.new(name)
+    puts "#{@player_1.name.upcase} will be X."
     puts "---"
-    puts "Player 2, enter your name:"
+    puts "Player 2, enter your Name:"
     name = gets.chomp
-    player_2 = Player.new(name)
-    puts "#{player_2.name.upcase} will be O."
+    @player_2 = Player.new(name)
+    puts "#{@player_2.name.upcase} will be O."
     play
   end
 end
